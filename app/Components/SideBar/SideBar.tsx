44b53d76cb1fa87d23f4f4be14d723893d53e4cc
bab882 <chanitebrandon@gmail.com>
2024-10-08 14:19:47 +0200

    commit Dark Mode

--- app/Components/SideBar/SideBar.tsx
+++ app/Components/SideBar/SideBar.tsx
@@ -4,14 +4,17 @@ import styled from 'styled-components';
 import { useGlobalState } from '@/app/context/globalProvider';
 
 function SideBar() {
-  const { theme } = useGlobalState();
 
-  console.log(theme);
-  return <SideBarStyled>SideBar</SideBarStyled>;
+  const { theme } = useGlobalState();
+  // console.log(theme);
+  return <SideBarStyled theme={theme}>SideBar</SideBarStyled>;
 }
 
 const SideBarStyled = styled.nav`
-   background-color:blue;
-   height: 10px;
+  position: relative;
+  width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border-right: 2px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;
 `;
 export default SideBar
